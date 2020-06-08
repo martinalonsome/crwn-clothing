54635d138cc46affb254eeecea50cca11a40120e
Martin Alonso <martinalonso4895@gmail.com>
2020-06-08 15:13:30 -0400

    added redux, implemented userReducer and userActions to header and app components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -57,7 +57,7 @@ class SignIn extends React.Component {
                     />
                     <div className="button-row">
                         <CustomButton type='submit'>Sign In</CustomButton>
-                        <CustomButton  onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
+                        <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
                     </div>
                 </form>
             </div>
--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import {Route} from 'react-router-dom';
 
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
 
 import './index.css';
 import App from './App';
 
+import store from './redux/store';
+
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, 
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+     </Provider>, 
     document.getElementById('root'))
 
