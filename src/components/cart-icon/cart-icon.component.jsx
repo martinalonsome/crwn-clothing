5cf1c66a382f9f02542e31d26c5b0d959e8009f1
Martin Alonso <martinalonso4895@gmail.com>
2020-06-10 14:35:50 -0400

    create cart item component and connect cart dropdown with cartItems

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -10,7 +10,7 @@ import './cart-icon.styles.scss';
 const CartIcon = ({toggleCartHidden}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'></span>
+        <span className='item-count'>0</span>
     </div>
 );
 
